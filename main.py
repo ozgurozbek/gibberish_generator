a680d95a3c81ade63387b9829efb7e7f4d4f9106
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2019-09-05 15:19:49 +0300

    Add decoder, fix structure

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-d = list(input(bytearray.fromhex("596f757220737472696e673a20").decode()).upper())
-o,n,m,a,a_=d,len(d),int(input(bytearray.fromhex("4c616e6775616765206e756d6265723a20").decode())),list("BCDFGHJKLMNPQRSTVWXYZ"),list("AEIOU")
-for i in range(n):
-    if d[i] in a:o[i]=a[(i+m)%21]
-    if d[i] in a_:o[i]=a_[(i+m)%5]
-print(''.join(o))
+#coder
+def encoder():
+  for i in range(n):
+    if d[i] in a:o[i]=a[(i+m+(a.index(d[i])))%21]
+    if d[i] in a_:o[i]=a_[(i+m+(a_.index(d[i])))%5]
+  print(''.join(o))
+
+#decoder
+def decoder():
+  for i in range(n):
+    if d[i] in a:o[i]=a[((a.index(d[i]))-m-i)%21]
+    if d[i] in a_:o[i]=a_[((a_.index(d[i]))-m-i)%5]
+  print(''.join(o))
+
+#loop
+while(1):
+  d = list(input(bytearray.fromhex("596f757220737472696e673a20").decode()).upper())
+  o,n,m,a,a_=d,len(d),int(input(bytearray.fromhex("4c616e6775616765206e756d6265723a20").decode())),list("BCDFGHJKLMNPQRSTVWXYZ"),list("AEIOU")
+
+  #for you to find out what was that word
+  print("1 for decode anything else for encode :> ")
+  decoder() if input()=="1" else encoder()
