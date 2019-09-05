5be22090da007223a566b7a8d2af98380c6c275c
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2019-09-05 15:19:49 +0300

    Update main.py
    Added an ugly paragraph support
    Add decoder, fix structure
    Convert the CodinGame Shortest stuff to readable code

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-d = list(input(bytearray.fromhex("596f757220737472696e673a20").decode()).upper())
-o,n,m,a,a_=d,len(d),int(input(bytearray.fromhex("4c616e6775616765206e756d6265723a20").decode())),list("BCDFGHJKLMNPQRSTVWXYZ"),list("AEIOU")
-for i in range(n):
-    if d[i] in a:o[i]=a[(i+m)%21]
-    if d[i] in a_:o[i]=a_[(i+m)%5]
-print(''.join(o))
+consonants = list("BCDFGHJKLMNPQRSTVWXYZ")
+vowels = list("AEIOU")
+language_number = int(input("Language number for this session: "))
+
+def encoder(word):
+  word = list(word)
+  for i in range(len(word)):
+    if word[i] in consonants:
+      word[i] = consonants[(i+language_number+(consonants.index(word[i])))%len(consonants)]
+    elif word[i] in vowels:
+      word[i] = vowels[(i+language_number+(vowels.index(word[i])))%len(vowels)]
+  a.append(''.join(word))
+
+def decoder(word):
+  word = list(word)
+  for i in range(len(word)):
+    if word[i] in consonants:
+      word[i] = consonants[((consonants.index(word[i]))-i-language_number)%len(consonants)]
+    elif word[i] in vowels:
+      word[i] = vowels[((vowels.index(word[i]))-i-language_number)%len(vowels)]
+  a.append(''.join(word))
+
+def processor(word_list, value):
+  if value == 0:
+    for i in word_list:
+      encoder(i)
+  elif value == 1:
+    for i in word_list:
+      decoder(i)
+
+while(True):
+  a = []
+  word_list = list(map(list, input("Enter string: ").upper().split()))
+  processor(word_list, 1) if input("Enter 1 for Decode, 0 for Encode: ") == "1" else processor(word_list, 0)
+  print(" ".join(a))
